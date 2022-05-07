32702df6479cfabf48ec8d0b1aa02d72f31eb5fe
Elvin Moises Sanchez Medina <emsanchez891@gmail.com>
2022-05-07 10:14:10 -0600

    fix: rem unit in loader css

--- src/components/UsersListRow/index.jsx
+++ src/components/UsersListRow/index.jsx
@@ -1,6 +1,6 @@
 import Loader from '../ui/Loader';
 
-const UsersListRow = ({ loading }) => {
+const UsersListRow = ({ loading = true }) => {
 	if (loading) return <Loader />;
 	return <div>Usuarios aca</div>;
 };
